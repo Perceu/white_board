facf77075d1d5a20014a770880401c549769f1a2
Perceu Bertoletti <perceubertoletti@gmail.com>
2019-06-09 19:18:54 -0300

    ajustes para melhorar o uso em 2 telas

--- run.py
+++ run.py
@@ -12,28 +12,38 @@ class Paint(object):
 
     def __init__(self):
         self.root = Tk()
+        self.action_frame = Frame(self.root)
+        self.action_frame.grid(row=0, column=0, sticky='wen')
 
-        self.pen_button_black = Button(self.root, text='B', bg=self.DEFAULT_BLACK, fg=self.DEFAULT_BLACK, command=self.use_pen_black)
+        self.pen_button_black = Button(self.action_frame, text='B', bg=self.DEFAULT_BLACK, fg=self.DEFAULT_BLACK, command=self.use_pen_black)
         self.pen_button_black.grid(row=0, column=0)
 
-        self.pen_button_red = Button(self.root, text='R', bg=self.DEFAULT_RED, fg=self.DEFAULT_RED, command=self.use_pen_red)
+        self.pen_button_red = Button(self.action_frame, text='R', bg=self.DEFAULT_RED, fg=self.DEFAULT_RED, command=self.use_pen_red)
         self.pen_button_red.grid(row=0, column=1)
 
-        self.pen_button_blue = Button(self.root, text='B', bg=self.DEFAULT_BLUE, fg=self.DEFAULT_BLUE, command=self.use_pen_blue)
+        self.pen_button_blue = Button(self.action_frame, text='B', bg=self.DEFAULT_BLUE, fg=self.DEFAULT_BLUE, command=self.use_pen_blue)
         self.pen_button_blue.grid(row=0, column=2)
 
-        self.pen_button_green = Button(self.root, text='G', bg=self.DEFAULT_GREEN, fg=self.DEFAULT_GREEN, command=self.use_pen_green)
+        self.pen_button_green = Button(self.action_frame, text='G', bg=self.DEFAULT_GREEN, fg=self.DEFAULT_GREEN, command=self.use_pen_green)
         self.pen_button_green.grid(row=0, column=3)
 
-        self.eraser_button = Button(self.root, text='apagador', command=self.use_eraser)
+        self.eraser_button = Button(self.action_frame, text='apagador', command=self.use_eraser)
         self.eraser_button.grid(row=0, column=4)
 
-        self.clean_button = Button(self.root, text='limpar', command=self.clean_canvas)
+        self.clean_button = Button(self.action_frame, text='limpar', command=self.clean_canvas)
         self.clean_button.grid(row=0, column=5)
 
-        self.c = Canvas(self.root, bg='white', width=600, height=600)
-        self.c.grid(row=1, columnspan=6)
+        self.browser_button = Button(self.action_frame, text='Navegador', command=self.drawn_browser)
+        self.browser_button.grid(row=0, column=6)
 
+        self.draw_frame = Frame(self.root)
+        self.draw_frame.grid(row=1, column=0, sticky='nwse')
+
+        self.c = Canvas(self.draw_frame, bg='white')
+        self.c.grid(row=0, sticky='nwse')
+
+        self.root.grid_rowconfigure(1, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
         self.setup()
         self.root.mainloop()
 
@@ -75,13 +85,20 @@ class Paint(object):
         if self.fullscreen:
             self.c.config(width=self.root.winfo_screenwidth(), height=self.root.winfo_screenheight())
         else:
-            self.c.config(width=600, height=600)
+            self.c.config(width=self.root.winfo_width(), height=self.root.winfo_height())
 
         self.root.attributes("-fullscreen", self.fullscreen)
 
     def use_eraser(self):
         self.line_width = self.ERASER_SIZE
         self.activate_button(self.eraser_button, eraser_mode=True)
+
+    def drawn_browser(self):
+        self.c.delete("all")
+        self.c.create_line(0, 20, self.root.winfo_width(), 20)
+        self.c.create_line(0, 60, self.root.winfo_width(), 60)
+        self.c.create_line(10, 0, 10, 20)
+        self.c.create_line(10, 60, self.root.winfo_width(), 60)
 
     def clean_canvas(self):
         self.c.delete("all")
