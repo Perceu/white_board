2bc5eaaf32322584938d2e87a4b441df802eeea6
Perceu Bertoletti <perceubertoletti@gmail.com>
2019-08-24 00:05:24 -0300

    add icons in whiteboard

--- run.py
+++ run.py
@@ -16,31 +16,46 @@ class Paint(object):
         self.action_frame = Frame(self.root)
         self.action_frame.grid(row=0, column=0, sticky='wen')
 
-        self.pen_button_black = Button(self.action_frame, text='B', bg=self.DEFAULT_BLACK, fg=self.DEFAULT_BLACK, command=self.use_pen_black)
+        self.pen_button_black = Button(self.action_frame, 
+            text='B', 
+            bg=self.DEFAULT_BLACK, fg=self.DEFAULT_BLACK, 
+            cursor="hand2", 
+            command=self.use_pen_black
+        )
         self.pen_button_black.grid(row=0, column=0)
 
-        self.pen_button_red = Button(self.action_frame, text='R', bg=self.DEFAULT_RED, fg=self.DEFAULT_RED, command=self.use_pen_red)
+        self.pen_button_red = Button(self.action_frame, 
+            text='R', 
+            bg=self.DEFAULT_RED, 
+            fg=self.DEFAULT_RED, 
+            cursor="hand2",
+            command=self.use_pen_red
+        )
         self.pen_button_red.grid(row=0, column=1)
 
-        self.pen_button_blue = Button(self.action_frame, text='B', bg=self.DEFAULT_BLUE, fg=self.DEFAULT_BLUE, command=self.use_pen_blue)
+        self.trashIcon = PhotoImage(file="./icons/trash.png")
+        self.eraseIcon = PhotoImage(file="./icons/eraser.png")
+        self.browserIcon = PhotoImage(file="./icons/browser.png")
+
+        self.pen_button_blue = Button(self.action_frame, text='B', cursor="hand2", bg=self.DEFAULT_BLUE, fg=self.DEFAULT_BLUE, command=self.use_pen_blue)
         self.pen_button_blue.grid(row=0, column=2)
 
-        self.pen_button_green = Button(self.action_frame, text='G', bg=self.DEFAULT_GREEN, fg=self.DEFAULT_GREEN, command=self.use_pen_green)
+        self.pen_button_green = Button(self.action_frame, text='G', cursor="hand2", bg=self.DEFAULT_GREEN, fg=self.DEFAULT_GREEN, command=self.use_pen_green)
         self.pen_button_green.grid(row=0, column=3)
 
-        self.eraser_button = Button(self.action_frame, text='apagador', command=self.use_eraser)
+        self.eraser_button = Button(self.action_frame, compound=CENTER, image=self.eraseIcon, width="25",height="25", text='', command=self.use_eraser)
         self.eraser_button.grid(row=0, column=4)
 
-        self.clean_button = Button(self.action_frame, text='limpar', command=self.clean_canvas)
+        self.clean_button = Button(self.action_frame, compound=CENTER, image=self.trashIcon, width="25",height="25", text='', command=self.clean_canvas)
         self.clean_button.grid(row=0, column=5)
 
-        self.browser_button = Button(self.action_frame, text='Navegador', command=self.drawn_browser)
+        self.browser_button = Button(self.action_frame, compound=CENTER, image=self.browserIcon, width="25",height="25", text='', command=self.drawn_browser)
         self.browser_button.grid(row=0, column=6)
 
         self.draw_frame = Frame(self.root)
         self.draw_frame.grid(row=1, column=0, sticky='nwse')
 
-        self.c = Canvas(self.draw_frame, bg='white')
+        self.c = Canvas(self.draw_frame, bg='white', cursor='cross')
         self.c.grid(row=0, sticky='nwse')
 
         self.root.grid_rowconfigure(1, weight=1)
@@ -57,6 +72,7 @@ class Paint(object):
         self.active_button = self.pen_button_black
         self.root.bind("<F12>", self.quit)
         self.root.bind("<F11>", self.toogle_fullscreen)
+        self.root.bind("<Escape>", self.toogle_fullscreen)
         self.root.bind("<F5>", self.clean_canvas)
         self.root.bind("<F1>", self.help)
         self.c.bind('<B1-Motion>', self.paint)
@@ -73,6 +89,7 @@ class Paint(object):
         messagebox.showinfo("Help","""
         <f12> Fecha o programa\n
         <f11> Tela Cheia\n
+        <Esc> Tela Cheia\n
          <f5> Limpa Tela\n
          <f1> Ajuda\n
         """)
